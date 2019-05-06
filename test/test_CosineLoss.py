9262231abadb68b64c7a5415c0600c9ffada1c4c
yatszhash <sushi.yatsuhashi@gamil.com>
2019-05-06 18:38:01 +0900

    fix: cosine loss with one hot encoding

--- test/test_CosineLoss.py
+++ test/test_CosineLoss.py
@@ -10,7 +10,7 @@ class TestCrossEntropyCosineLoss(TestCase):
     def test(self):
         loss_function = CrossEntropyCosineLoss(lambda_=0.3)
         inputs_ = torch.Tensor([[0.99, 0.89], [0.55, 0.33]])
-        labels_ = torch.Tensor([[0., 1.], [1., 0.]])
+        labels_ = torch.LongTensor([0, 1])
         output_ = loss_function(inputs_, labels_)
         print(output_)
         # TODO assertion
@@ -20,7 +20,7 @@ class TestCosineLoss(TestCase):
     def test(self):
         loss_function = CosineLoss()
         inputs_ = torch.Tensor([[0.99, 0.01], [0.55, 0.33]])
-        labels_ = torch.Tensor([[0., 1.], [1., 0.]])
+        labels_ = torch.LongTensor([[1], [0]])
         output_ = loss_function(inputs_, labels_)
         print(output_)
         # TODO assertion
